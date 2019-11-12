6b59878262f532322cec684d88f4a9da25d531cd
AsteriskAmpersand <aravuegnitsuga@gmail.com>
2019-11-12 09:49:37 -0500

    CCL is now Error Tolerant
    Extended the CCL Importer to be error tolerant.
    -Allows truncating, aborting or nulling capsules with mismatched IDs.
    -Warns about corrupted CCL instead of printing the stack trace.

--- operators/cclimport.py
+++ operators/cclimport.py
@@ -28,19 +28,66 @@ class ImportCCL(Operator, ImportHelper):
         description = "Multiply sphere radii (Factor of 2 according to Statyk)",
         default = 1.0)    
 
+    missingFunctionBehaviour = EnumProperty(
+            name = "Missing Bone Functions",
+            description = "Determines what to do while opening a file with missing bone functions",
+            items = [("Abort","Abort","Aborts importing process",0),
+                     ("Omit","Omit","Omit the entire sphere",1),
+                     ("Null","Null","Sets the constraint target to null",2)],
+            default = "Null"
+            )    
+    
+    def cleanup(self,obj):
+        for children in obj.children:
+            self.cleanup(children)
+        objs = bpy.data.objects
+        objs.remove(objs[obj.name], do_unlink=True)
+
+    @staticmethod
+    def showMessageBox(message = "", title = "Message Box", icon = 'INFO'):
+    
+        def draw(self, context):
+            self.layout.label(message)
+    
+        bpy.context.window_manager.popup_menu(draw, title = title, icon = icon)
+
+    def displayErrors(self, errors):
+        if errors:
+            for _ in range(20):print()
+            print("CCL Import Errors:")
+            print("#"*75)
+            print(errors)
+            print("#"*75)
+            if self.missingFunctionBehaviour == "Abort": message= "Import Process aborted due to error, check the reason in Window > Toggle_System_Console"
+            else: message="Warnings have been Raised, check them in Window > Toggle_System_Console"
+            self.showMessageBox(message, title = "Warnings and Error Log")
+    
     def execute(self,context):
         try:
             bpy.ops.object.mode_set(mode='OBJECT')
         except:
             pass
+        self.ErrorMessages = []
         bpy.ops.object.select_all(action='DESELECT')
-        ccl = CclFile(self.properties.filepath)
+        try: ccl = CclFile(self.properties.filepath)
+        except:
+            self.ErrorMessages.append("Corrupted CCL File couldn't be read.")
+            self.missingFunctionBehaviour = "Abort"
+            self.displayErrors(self.ErrorMessages)
+            return {'FINISHED'}
+        capsules = []
         for ix, entry in enumerate(ccl.data):
             try:
                 capsule = self.recordToCapsule(entry)
-                capsule.name = "%d Capsule"%ix
-            except Exception as e: 
-                print(e)                
+                capsule.name = "%d Capsule"%ix  
+                capsules.append(capsule)
+            except:
+                if self.missingFunctionBehaviour == "Omit":
+                    pass
+                elif self.missingFunctionBehaviour == "Abort":
+                    for capsule in capsules: self.cleanup(capsule)
+                    break
+        self.displayErrors(self.ErrorMessages)
         return {'FINISHED'}
 
     def recordToCapsule(self, record):
@@ -50,8 +97,16 @@ class ImportCCL(Operator, ImportHelper):
         r2,co2 = record.endsphere_radius*self.scale, Vector([record.endsphere_xOffset,
                                                   record.endsphere_yOffset, 
                                                   record.endsphere_zOffset])
-        f1 = findFunction(record.boneIDOne)
-        f2 = findFunction(record.boneIDTwo)
+        try: f1 = findFunction(record.boneIDOne)
+        except:
+            self.ErrorMessages.append("CCL Points to Bone Function %d but missing from the Skeleton."%record.boneIDOne)
+            if self.missingFunctionBehaviour == "Null": f1=None 
+            else: raise ValueError()
+        try: f2 = findFunction(record.boneIDTwo)
+        except: 
+            self.ErrorMessages.append("CCL Points to Bone Function %d but missing from the Skeleton."%record.boneIDTwo)
+            if self.missingFunctionBehaviour == "Null": f1=None 
+            else: raise ValueError()
         return createCapsule(f1,f2,r1,r2,co1,co2)
     
     
